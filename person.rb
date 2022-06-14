c2cab35a4bc0b9615d72789db2d5c7782e09a54c
levy002 <levy.ukwi002@gmail.com>
2022-06-14 14:23:53 +0200

    Correcting spelling mistake

--- person.rb
+++ person.rb
@@ -1,6 +1,6 @@
 require './nameable'
 
-class Person < nameable
+class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1..100)
