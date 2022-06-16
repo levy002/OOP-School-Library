c85ed24f10342b5b0df4876c0449c8ad7ac0fbdc
levy002 <levy.ukwi002@gmail.com>
2022-06-16 15:15:33 +0200

    Add Associations

--- person.rb
+++ person.rb
@@ -1,17 +1,17 @@
 require './nameable'
 
 class Person < Nameable
+  attr_reader :id, :rentals
+  attr_accessor :name, :age
+  
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1..100)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
-
-  attr_reader :id
-
-  attr_accessor :name, :age
 
   def can_use_services?
     true if @age >= 18 || parent_permission == true
--- student.rb
+++ student.rb
@@ -1,9 +1,9 @@
 require './person'
 
 class Student < Person
-  def initialize(age, classroom, name, parent_permission: true)
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
-    @classroom = classroom
+    classroom.add_student(self) unless classroom.includes?(self)
   end
 
   def play_hooky
